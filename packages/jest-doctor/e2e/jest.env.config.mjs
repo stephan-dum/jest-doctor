dbb828485bc7f9795203f553ce300ec43134273c
Stephan <s.dum@netconomy.net>
2026-01-23 16:59:59 +0100

    clean up

--- packages/jest-doctor/e2e/jest.env.config.mjs
+++ packages/jest-doctor/e2e/jest.env.config.mjs
@@ -3,6 +3,9 @@ export default {
   transform: {
     '^.+\\.(tsx?|m?js)$': 'ts-jest',
   },
+  fakeTimers: {
+    enableGlobally: true,
+  },
   preset: 'ts-jest',
   maxWorkers: 1,
   maxConcurrency: 1,

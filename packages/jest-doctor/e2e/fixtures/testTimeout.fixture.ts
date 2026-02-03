d2f09c6c1f34fd6ae7dbb7ea42bd098599f9a8ed
Stephan <s.dum@netconomy.net>
2026-02-03 01:58:51 +0100

    feat/jd-9 add test

--- packages/jest-doctor/e2e/fixtures/testTimeout.fixture.ts
+++ packages/jest-doctor/e2e/fixtures/testTimeout.fixture.ts
@@ -1,5 +1,8 @@
 import { setTimeout } from 'node:timers';
 
+afterEach(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+}, 10);
 it('should timeout', async () => {
   await new Promise((resolve) => setTimeout(resolve, 100));
 }, 10);
--- packages/jest-doctor/e2e/tests/testTimeout.test.ts
+++ packages/jest-doctor/e2e/tests/testTimeout.test.ts
@@ -2,8 +2,9 @@ import { runTest } from '../runJest';
 
 it('should timeout the test', async () => {
   const result = await runTest(`testTimeout.fixture.ts`);
-  expect(
-    result.testResults[0].assertionResults[0].failureMessages[0],
-  ).toContain('Exceeded timeout of');
+  const failureMessages =
+    result.testResults[0].assertionResults[0].failureMessages;
+  expect(failureMessages[0]).toContain('Exceeded timeout of 10ms for a test');
+  expect(failureMessages[1]).toContain('Exceeded timeout of 10ms for a hook');
   expect(result.success).toEqual(false);
 });

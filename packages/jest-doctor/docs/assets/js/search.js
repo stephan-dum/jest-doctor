3f81b903ab567f8900e68467bf8daff842a81ba9
Stephan <s.dum@netconomy.net>
2026-02-03 03:03:53 +0100

    docs algolia search

---
+++ packages/jest-doctor/docs/assets/js/search.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+
+const searchClient = algoliasearch(
+  '5DXOBHA2ZD',
+  'cbbe74e664008bf002e0f6edcc0942ee',
+);
+
+const search = instantsearch({
+  indexName: 'stephan_dum_github_io_5dxobha2zd_pages',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#search-container',
+    placeholder: 'Search the docs…',
+    cssClasses: {
+      root: 'search-box', // adds a wrapper class for styling
+    },
+  }),
+
+  instantsearch.widgets.stats({
+    container: '#search-container',
+    cssClasses: {
+      root: 'search-stats',
+    },
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#search-container',
+    templates: {
+      item(hit, { html, components }) {
+        return html` <article>
+          <a href="${hit.url}">
+            <h2>${components.Highlight({ attribute: 'title', hit })}</h2>
+            <p>${components.Snippet({ attribute: 'content', hit })}</p>
+          </a>
+        </article>`;
+      },
+    },
+    cssClasses: {
+      root: 'search-hits',
+    },
+  }),
+]);
+
+search.start();
